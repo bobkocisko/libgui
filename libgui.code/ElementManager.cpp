556feb58938200be2049db3d3d7ea0cb526c142c
bobkocisko <rkocisko@gmail.com>
2015-04-12 22:52:42 -0400

    ElementManager hit tests against all elements

--- libgui.code/ElementManager.cpp
+++ libgui.code/ElementManager.cpp
@@ -25,4 +25,77 @@ namespace libgui
 		m_capturedControl->NotifyReleasingCapture();
 		m_capturedControl = nullptr;
 	}
+
+	bool ElementManager::NotifyMouseMove(int x, int y)
+	{
+		bool needsUpdate = false;
+
+		if (m_activeControl)
+		{
+			if (!m_activeControl->HitTest( Location { x, y }))
+			{
+				m_activeControl->NotifyLeave();
+				m_activeControl = NULL;
+				needsUpdate = true;
+			}
+		}
+
+		for (auto &element : m_elements)
+		{
+			if (element->HitTest(Location{ x, y }))
+			{
+				auto control = dynamic_pointer_cast<Control> (element);
+				if (m_activeControl != control)
+				{
+					if (m_activeControl)
+					{
+						m_activeControl->NotifyLeave();
+					}
+
+					control->NotifyEnter();
+					m_activeControl = control;
+					needsUpdate = true;
+				}
+
+				break;
+			}
+		}
+
+		return needsUpdate;
+	}
+
+	bool ElementManager::NotifyMouseDown(int x, int y)
+	{
+		bool needsUpdate;
+
+		// Just in case the mouse move event was never propagated
+		// for this x and y position
+		needsUpdate = NotifyMouseMove(x, y);
+
+		if (m_activeControl)
+		{
+			m_activeControl->NotifyDown();
+			needsUpdate = true;
+		}
+
+		return needsUpdate;
+	}
+
+	bool ElementManager::NotifyMouseUp(int x, int y)
+	{
+		bool needsUpdate;
+
+		// Just in case the mouse move event was never propagated
+		// for this x and y position
+		needsUpdate = NotifyMouseMove(x, y);
+
+		if (m_activeControl)
+		{
+			m_activeControl->NotifyUp();
+			needsUpdate = true;
+		}
+
+		return needsUpdate;
+	}
+
 }
--- libgui.code/ElementManager.h
+++ libgui.code/ElementManager.h
@@ -12,6 +12,10 @@ namespace libgui
 
 		void AddElement(shared_ptr<Element>);
 
+		bool NotifyMouseMove(int x, int y);
+		bool NotifyMouseDown(int x, int y);
+		bool NotifyMouseUp(int x, int y);
+
 		void RequestCapture(shared_ptr<Control>);
 		void ReleaseCapture();
 	private:
@@ -23,6 +27,14 @@ namespace libgui
 		void operator=(ElementManager const&) = delete;
 
 		shared_ptr<Control> m_capturedControl = nullptr;
+		
+		// The active control is the control that was most
+		// recently sent a NotifyEnter nofication.
+		shared_ptr<Control> m_activeControl = nullptr;
+
+		// The elements list contains all elements that
+		// this element manager is reponsible for,
+		// ordered in Z-order ascending (top-most elements last)
 		vector<shared_ptr<Element>> m_elements;
 	};
 }
